d33e4272ec27daa4ff811d6c1d5fc036533f3298
Nullptr <tsng.nullptr@outlook.com>
2021-08-01 13:00:04 +0800

    feat: update help information

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,8 +47,8 @@ android {
             cppFlags += "-std=c++20"
         }
 
-        versionCode = 59
-        versionName = "2.0.9"
+        versionCode = 60
+        versionName = "2.1"
         buildConfigField("int", "SERVICE_VERSION", "59")
         buildConfigField("int", "MIN_RIRU_VERSION", "23")
         buildConfigField("int", "MIN_BACKUP_VERSION", "49")
