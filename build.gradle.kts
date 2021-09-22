8c4615bea69345225802614cecd6e8d30da8b026
Nullptr <tsng.nullptr@outlook.com>
2021-09-22 17:59:05 +0800

    chore: update gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:7.1.0-alpha08")
+        classpath("com.android.tools.build:gradle:7.1.0-alpha12")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
     }
 }
